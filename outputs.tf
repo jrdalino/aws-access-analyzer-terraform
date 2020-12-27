358003aa9e48639a33e95982b9f555620a7e0267
Jose Dalino <jrdalino@gmail.com>
2020-12-27 18:42:49 +0800

    Initial

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Access Analyzer
 output "aws_accessanalyzer_analyzer_id" {
-  value       = aws_accessanalyzer_analyzer_id.this.id
+  value       = aws_accessanalyzer_analyzer.this.id
   description = "Analyzer name."
 }
